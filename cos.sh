b1e5582e51b6445e70823593e2bdbab9090712cb
residentevilfreak <112755412+residentevilfreak@users.noreply.github.com>
2026-02-11 03:00:06 +0500

    Add openSUSE setup script
    This script installs essential packages and configures GRUB settings for openSUSE.

---
+++ cos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+exec < /dev/tty
+
+echo "=== opensuse setup script ==="
+
+PACMAN_PKGS=(
+flatpak
+filezilla
+qbittorrent
+steam
+lutris
+wine
+mangohud
+goverlay
+gamemode
+vlc
+)
+
+sudo pacman -Syu --needed "${PACMAN_PKGS[@]}"
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.bitwarden.desktop com.surfshark.Surfshark com.obsproject.Studio com.discordapp.Discord
+
+sudo sed -i \
+-e 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=-1/' \
+/etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "=== setup complete! ==="
