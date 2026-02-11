d4d2442ab5ad14efc319dd8e41adb92b881c0c90
residentevilfreak <112755412+residentevilfreak@users.noreply.github.com>
2026-02-11 23:32:39 +0500

    Install music-presence-bin
    Add music-presence-bin installation

--- cos.sh
+++ cos.sh
@@ -22,6 +22,8 @@ sudo pacman -Syu --needed "${PACMAN_PKGS[@]}"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.bitwarden.desktop com.surfshark.Surfshark com.obsproject.Studio com.discordapp.Discord com.spotify.Client
 
+paru -S music-presence-bin
+
 sudo sed -i \
 -e 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=-1/' \
 /etc/default/grub
