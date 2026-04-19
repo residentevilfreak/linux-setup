2496b928217d2d0aa595b40859846c85abaa5f2e
residentevilfreak <112755412+residentevilfreak@users.noreply.github.com>
2026-04-20 02:03:32 +0500

    Add betterbird-bin to installation list in cos.sh

--- cos.sh
+++ cos.sh
@@ -24,7 +24,7 @@ sudo pacman -Syu --needed "${PACMAN_PKGS[@]}"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.obsproject.Studio com.discordapp.Discord dev.vencord.Vesktop com.spotify.Client org.kde.marknote com.github.tchx84.Flatseal
 
-paru -S --noconfirm music-presence-bin amneziavpn-bin 
+paru -S --noconfirm music-presence-bin amneziavpn-bin betterbird-bin
 
 paru -S polychromatic
 
