6a1f235f0c6851f681457b580f070488e4478084
residentevilfreak <112755412+residentevilfreak@users.noreply.github.com>
2026-06-13 13:59:19 +0500

    Add cachyOS setup script for package installation

---
+++ main/cos.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+exec < /dev/tty
+
+echo "=== cachyOS setup script ==="
+
+PACMAN_PKGS=(
+vlc
+discord
+vesktop
+kweather
+gnome-maps
+filezilla
+qbittorrent
+prismlauncher
+flatpak
+ntfs-3g
+gamemode
+protonup-qt
+protontricks
+cachyos-gaming-meta
+cachyos-gaming-applications
+)
+
+sudo pacman -Syu --needed "${PACMAN_PKGS[@]}"
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.obsproject.Studio com.spotify.Client org.kde.marknote com.github.tchx84.Flatseal app.zen_browser.zen
+
+paru -S --noconfirm music-presence-bin amneziavpn-bin betterbird-bin
+
+paru -S polychromatic
+
+sudo sed -i \
+-e 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=-1/' \
+-e 's/^#*GRUB_DISABLE_OS_PROBER=.*/GRUB_DISABLE_OS_PROBER=false/' \
+/etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+sudo gpasswd -a $USER openrazer
+
+wpctl settings --save bluetooth.autoswitch-to-headset-profile false
+
+systemctl --user restart wireplumber
+
+echo "=== setup complete! ==="
